c83855355264e9526d06e578bbcbd12cde20725f
Abdella Solomon <abdesoltak@gmail.com>
2024-06-21 12:03:07 +0300

    a change in the about

--- components/about.js
+++ components/about.js
@@ -7,12 +7,10 @@ const About = ({ id }) => {
       <Title title="About"/>
       <div className="flex flex-col items-center justify-center md:flex-row md:justify-start md:items-start">
         <p className="grow font-quicksand text-xl text-textDark md:w-1/2">
-          I am a programmer since July 2020 and I have been working
-          with many programming languages and frameworks.
+          I am a .NET developer specializing in desktop applications, and I am an aspiring backend engineer.
           <br />
-          Additionally, I do mathematics stuff in my free time and more
-          specifically, I am focused on Artifical Intelligence and machine
-          learning. I love to learn new things and adapt to new technologies.
+          Additionally, I do automations in python and learn mathematics as a hobby. I love to learn and adapt
+          to new technologies.
         </p>
         <div className="relative mx-10 mt-5  md:-mt-12">
           <Image src="/logo.png" width={300} height={300} />
